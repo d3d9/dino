0f086fd110bf7a9cc99fe2a0dbb81ff2007deb87
d3d9 <d3d9@riseup.net>
2018-02-20 14:52:28 +0100

    also use ./dino/ directory in dinotocsv.py

--- dinotocsv.py
+++ dinotocsv.py
@@ -8,20 +8,20 @@ from datetime import date, datetime, time, timedelta
 
 betrieb = 12 #HST
 
-with open("./rec_lin_ber.din", 'r') as linefile:
+with open("./dino/rec_lin_ber.din", 'r') as linefile:
 	rec_lin_ber = pandas.read_csv(linefile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'LINE_NR':int,'STR_LINE_VAR':int,'LINE_DIR_NR':int,'LINE_NAME':str}, index_col=3)
-with open("./rec_stop.din", 'r') as stopfile:
+with open("./dino/rec_stop.din", 'r') as stopfile:
 	rec_stop = pandas.read_csv(stopfile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'STOP_NR':int}, index_col=1)
 	rec_stop = rec_stop[~rec_stop.index.duplicated(keep='first')] # weg hiermit, betrieb bevorzugen
-with open("./rec_stopping_points.din", 'r') as platfile:
+with open("./dino/rec_stopping_points.din", 'r') as platfile:
 	rec_stopping_points = pandas.read_csv(platfile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'STOP_NR':int,'STOPPING_POINT_NR':int,'IFOPT':str})
-with open("./service_restriction.din", 'r') as restrictionfile:
+with open("./dino/service_restriction.din", 'r') as restrictionfile:
 	service_restriction = pandas.read_csv(restrictionfile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'RESTRICTION':str,'RESTRICT_TEXT1':str,'RESTRICTION_DAYS':str})
-with open("./rec_trip.din", 'r') as tripfile:
+with open("./dino/rec_trip.din", 'r') as tripfile:
 	rec_trip = pandas.read_csv(tripfile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'LINE_NR':int,'STR_LINE_VAR':int,'LINE_DIR_NR':int,'DEPARTURE_TIME':int,'DEP_STOP_NR':int,'ARR_STOP_NR':int,'DAY_ATTRIBUTE_NR':int,'RESTRICTION':str})
-with open("./lid_course.din", 'r') as coursefile:
+with open("./dino/lid_course.din", 'r') as coursefile:
 	lid_course = pandas.read_csv(coursefile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'STOP_NR':int,'LINE_NR':int,'STR_LINE_VAR':int,'LINE_DIR_NR':int,'LINE_CONSEC_NR':int,'STOPPING_POINT_NR':int})
-with open("./lid_travel_time_type.din", 'r') as timefile:
+with open("./dino/lid_travel_time_type.din", 'r') as timefile:
 	lid_travel_time_type = pandas.read_csv(timefile, skipinitialspace=True, sep=';', dtype={'VERSION':int,'LINE_NR':int,'STR_LINE_VAR':int,'LINE_DIR_NR':int,'LINE_CONSEC_NR':int})
 
 zeit = timedelta()
